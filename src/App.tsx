152d2432d3822f7a418322305c38c67e2829f458
usernameistai <battye_david@yahoo.co.uk>
2026-06-19 20:53:30 +0100

    getting there

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import CryptoChart from "./components/CryptoChart";
 import { LuSquareMenu } from "react-icons/lu";
 
 const CryptoField = ({ label, value, subMetric }: { label: string, value: string, subMetric?: number }) => (
-  <div tabIndex={0} className="border-b-2 border-r-2 border-neutral-200/70 shadow-md p-2.5 md:p-4 rounded-md transition-[background-color,transform,box-shadow] duration-300 ease-in-out hover:bg-white/30 hover:scale-115 focus:bg-white/30 focus:scale-115">
-    <div className="text-[9px] md:text-[11px] font-semibold text-slate-500 uppercase tracking-tighter mb-1">{label}</div>
-    <div className="text-xs md:text-base font-bold text-slate-700/80 font-mono">{value}</div>
+  <div tabIndex={0} className="border-b-2 border-r-2 border-neutral-200/70 shadow-md p-4 md:p-4 rounded-md transition-[background-color,transform,box-shadow] duration-300 ease-in-out hover:bg-white/30 hover:scale-115 focus:bg-white/30 focus:scale-115">
+    <div className="text-[11px] md:text-[14px] font-semibold text-slate-500 uppercase tracking-tight mb-1">{label}</div>
+    <div className="text-sm md:text-base font-bold text-slate-700/80 font-mono tracking-tight">{value}</div>
     {subMetric !== undefined && (
-      <div  className={`text-[9px] md:text-sm font-semibold ${subMetric >= 0 ? 'text-emerald-400' : 'text-red-600'}`}>
+      <div  className={`text-[11px] md:text-sm font-semibold ${subMetric >= 0 ? 'text-emerald-400' : 'text-red-600'}`}>
         {subMetric >= 0 ? '▲' : '▼'} {Math.abs(subMetric)}
       </div>
     )}
@@ -131,13 +131,15 @@ const App: FC<CryptoDataProps> = () => {
         <div className="min-h-screen bg-neutral-200/20 antialiased overflow-x-hidden">
 
           {isLoading && (
-            <div className="fixed inset-0 w-screen h-screen bg-neutral-900/80 flex items-center justify-center z-9999 backdrop-blur-sm text-white">
+            <div className="fixed inset-0 w-screen h-screen bg-neutral-900/80 
+            flex items-center justify-center z-9999 backdrop-blur-sm text-white">
               <p className="animate-pulse font-mono tracking-[0.3em] uppercase">
                 Syncing Mission Data...
               </p>
             </div>
           )}
-          <div className="flex bg-white/5 backdrop-blur-md border border-white/10 rounded-md p-1 shadow-[0_4px_30px_rgba(0,0,0,0.1)] justify-between">
+          <div className="flex bg-white/5 backdrop-blur-md border border-white/10
+           rounded-md p-1 shadow-[0_4px_30px_rgba(0,0,0,0.1)] justify-between">
             {themeConfig.map((theme, idx) => (
               <button
                 key={theme.label}
@@ -156,7 +158,8 @@ const App: FC<CryptoDataProps> = () => {
             ))}
           </div>
           
-          <h1 className="top-0 mt-3 mb-10 md:my-3 text-center text-[#808080] text-xl md:text-4xl uppercase font-black tracking-[0.225em]">
+          <h1 className="top-0 mt-3 mb-10 md:my-3 text-center text-[#808080]
+           text-xl md:text-4xl uppercase font-black tracking-[0.225em]">
             Aegis Crypto Dashboard
           </h1>
 
@@ -169,7 +172,9 @@ const App: FC<CryptoDataProps> = () => {
             />
             <label 
               htmlFor="menu-toggle" 
-              className="touch-manipulation md:hidden p-2 fixed top-18 left-2 z-50 bg-neutral900/50 backdrop-blur-sm border border-white/10 text-teal-500 rounded-lg cursor-pointer flex items-center gap-2"
+              className="touch-manipulation md:hidden p-2 fixed top-18 left-2 z-50 
+              bg-neutral900/50 backdrop-blur-sm border border-white/10 text-teal-500
+               rounded-lg cursor-pointer flex items-center gap-2"
             >
               <div><LuSquareMenu size={24}/></div> 
               <div className="ml-1 font-mono font-semibold uppercase tracking-wider">Crypto Sidebar</div>
@@ -182,39 +187,20 @@ const App: FC<CryptoDataProps> = () => {
                shadow-[#808080]/70 shrink-0 p-2 md:p-4 m-2 md:m-4 rounded-lg
               overflow-y-auto touch-pan-y"
             > { /*  added overflow-y-auto overflow-scroll */ }
-              <div className="relative pb-4 mb-4 border-b border-mist-900/20 uppercase text-left font-semibold">
+
+              <div className="relative pb-4 mb-2 border-b border-mist-900/20 
+                uppercase text-left font-semibold">
                 <h2 className="text-slate-700/80 text-sm md:text-base">Crypto // Assets</h2>
               </div>
 
-              <div className="flex flex-col gap-y-2 mt-4 text-slate-700/80">
-                {coins.slice(0, 10).map((c) => (
-                  <button 
-                    key={c.id}
-                    className="uppercase my-1 px-0.5 md:px-4 py-2 text-left text-[12px] md:text-base font-semibold border border-mist-400/10 rounded-lg hover:text-white hover:bg-neutral-700/20 hover:border-mist-700/50"
-                    onClick={() => {
-                      setSelectedCoin(c);
-                      setParams((prev) => ({ ...prev, id: c.id }));
-
-                      if (menuRef.current) menuRef.current.checked = false;
-                    }}  
-                  >
-                    <h2>{c.id}</h2>
-                  </button>
-                ))}
-              </div>
-              
               <form
                 className="relative"
                 onSubmit={(e) => {
                   e.preventDefault();
                   
-                  if (filteredCoins.length > 0) {
-                    handleSelectCoin(filteredCoins[0]);
-                  };
+                  if (filteredCoins.length > 0) handleSelectCoin(filteredCoins[0]);
 
-                  if (menuRef.current) {
-                    menuRef.current.checked = false;
-                  };
+                  if (menuRef.current) menuRef.current.checked = false;
 
                   setSearch('');
                 }}
@@ -225,12 +211,17 @@ const App: FC<CryptoDataProps> = () => {
                   name="search" 
                   id="search" 
                   placeholder="Enter Crypto Coin"
-                  className="bg-neutral-100 px-2 md:px-4 py-1 md:py-2 my-2 md:my-4 text-[11px] md:text-base border border-neutral-300/50 focus:outline-none focus:ring-2 focus:ring-cyan-500 rounded-sm w-full inset-shadow-xl inset-shadow-black"
+                  className="bg-neutral-100 px-2 md:px-4 py-1 md:py-2 my-2 text-[11px]
+                  md:text-base border border-neutral-300/50 focus:outline-none focus:ring-2
+                   focus:ring-cyan-500 rounded-sm w-full inset-shadow-xl inset-shadow-black"
                   onChange={(e) => setSearch(e.target.value)}
                 />
                 <button
                   type="submit"
-                  className="px-4 py-2 mb-4 text-neutral-100 text-[10px] md:text-[17.5px] uppercase bg-teal-600 hover:bg-teal-500 rounded-sm w-full tracking-wider shadow-lg/30 hover:shadow-none hover:translate-y-0.5 focus:translate-y-0.5 focus:shadow-none"
+                  className="px-4 py-2 mb-4 text-neutral-100 text-[11px] md:text-[17.5px] 
+                  uppercase font-semibold bg-teal-500 hover:bg-teal-500/80 rounded-sm w-full 
+                  tracking-wider shadow-lg/30 hover:shadow-none hover:translate-y-0.5 
+                  focus:translate-y-0.5 focus:shadow-none"
                   onClick={() => {
                     if (menuRef.current) menuRef.current.checked = false;
                   }}
@@ -239,7 +230,8 @@ const App: FC<CryptoDataProps> = () => {
                 </button>
                 {/* Only show the list if the user has typed something */}
                 {search && filteredCoins.length > 0 && (
-                  <div className="absolute z-100 w-[88%] bg-neutral-800 text-white border border-neutral-600 -mt-2.5 max-h-60 rounded-sm shadow-xl overflow-y-auto">
+                  <div className="absolute z-100 w-[88%] bg-neutral-800 text-white border
+                   border-neutral-600 -mt-2.5 max-h-60 rounded-sm shadow-xl overflow-y-auto">
                     {filteredCoins.map((coin) => (
                       <button
                         type="button"
@@ -256,9 +248,31 @@ const App: FC<CryptoDataProps> = () => {
                   </div>
                 )}
               </form>
+
+              <div className="flex flex-col gap-y-2 text-slate-700/80">
+                {coins.slice(0, 15).map((c) => (
+                  <button 
+                    key={c.id}
+                    className="uppercase my-1 px-0.5 md:px-4 py-2 text-left text-[12px] 
+                    md:text-base font-semibold border-[1.5px] border-mist-400/10 rounded-lg
+                     hover:text-white hover:bg-neutral-700/20 hover:border-mist-100/50"
+                    onClick={() => {
+                      setSelectedCoin(c);
+                      setParams((prev) => ({ ...prev, id: c.id }));
+
+                      if (menuRef.current) menuRef.current.checked = false;
+                    }}  
+                  >
+                    <h2>{c.id}</h2>
+                  </button>
+                ))}
+              </div>
+              
             </aside>
 
-            <main className="min-h-screen col-span-12 md:col-span-9 lg:col-span-10 bg-[#808080]/10 backdrop-blur-md border-[1.5px] border-white/20 shadow-xl shadow-[#808080]/70 shrink-0 p-2 md:p-4 m-2 md:m-4 rounded-lg">
+            <main className="min-h-screen col-span-12 md:col-span-9 lg:col-span-10
+             bg-[#808080]/10 backdrop-blur-md border-[1.5px] border-white/20 
+             shadow-xl shadow-[#808080]/70 shrink-0 p-2 md:p-4 m-2 md:m-4 rounded-lg">
               <div className="pb-4 uppercase text-left font-semibold">
                 {selectedCoin ? (
                   <>
@@ -266,15 +280,15 @@ const App: FC<CryptoDataProps> = () => {
                     <h1 className="pb-4 mb-4 text-sm md:text-base border-b border-mist-900/20 text-slate-700/80">
                       Aegis Crypto - {selectedCoin.name} ({selectedCoin.symbol.toUpperCase()}) Databoard
                     </h1>
-                    <div className="bg-[#808080]/20 p-2.5 md:p-5 rounded-lg shadow-lg shadow-neutral-500/50">
+                    <div className="bg-white/5 p-2.5 md:p-5 rounded-lg shadow-lg shadow-neutral-500/50">
                       <div className="border-b-2 border-neutral-600/70 pb-5 mb-5 flex justify-between items-end">
                         <div>
-                          <h1 className="text-base md:text-3xl font-black text-white uppercase tracking-wide md:tracking-tighter">{selectedCoin.name}</h1>
-                          <p className="text-[10px] md:text-sm text-teal-700/70 font-black uppercase tracking-wide">{selectedCoin.id} // {selectedCoin.symbol.toUpperCase()}</p>
+                          <h1 className="text-lg md:text-3xl font-black text-white uppercase tracking-wide md:tracking-tighter">{selectedCoin.name}</h1>
+                          <p className="text-[11px] md:text-sm text-teal-700/70 font-black uppercase tracking-wide">{selectedCoin.id} // {selectedCoin.symbol.toUpperCase()}</p>
                         </div>
                         <div className="text-right">
-                          <div className="text-base md:text-3xl font-black text-white">£{`${selectedCoin.current_price <= 3 ? selectedCoin.current_price : selectedCoin.current_price.toLocaleString()}`}</div>
-                          <div className="text-[10px] md:text-sm font-black text-teal-700/70 uppercase tracking-wide">Current Price</div>
+                          <div className="text-lg md:text-3xl font-black text-white">£{`${selectedCoin.current_price <= 3 ? selectedCoin.current_price : selectedCoin.current_price.toLocaleString()}`}</div>
+                          <div className="text-[11px] md:text-sm font-black text-teal-700/70 uppercase tracking-wide">Current Price</div>
                         </div>
                       </div>
 
@@ -294,7 +308,7 @@ const App: FC<CryptoDataProps> = () => {
                       {[7, 30, 90].map((day) => (
                         <button
                           key={day}
-                          className={`px-4 md:px-7 py-1 md:py-1.5 mx-auto md:mx-0 rounded-full border text-xs ${
+                          className={`px-5 md:px-7 py-1 md:py-1.5 mx-auto md:mx-0 rounded-full border text-xs ${
                             params.days === day
                               ? 'bg-neutral-800 text-white'
                               : 'bg-white/50 text-neutral-600 hover:bg-neutral-200'
